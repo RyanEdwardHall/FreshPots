fa94e7b56783bc379b57c35b7f9d4221f5c07892
Ryan Hall <ryan.hall@vidyard.com>
2015-04-23 12:36:35 -0400

    slack integration

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ ruby "2.2.1"
 
 gem 'pi_piper'          # https://github.com/jwhitehorn/pi_piper
 gem 'sonos'             # https://github.com/soffes/sonos
+gem 'curb'              # https://github.com/taf2/curb
 # gem 'eventmachine'    # to replace deprecated pi_piper wait loop
--- lib/freshest-pot.rb
+++ lib/freshest-pot.rb
@@ -1,15 +1,17 @@
 # require 'eventmachine'
 require 'pi_piper'
 require 'sonos'
+require 'curb'
 include PiPiper
 
 # Speaker setup
-system = Sonos::System.new
-speaker = system.speakers.first
+# system = Sonos::System.new
+# speaker = system.speakers.first
 
-after :pin => 23, :goes => :high do
-  pp "Pin's hot!"
+after :pin => 4, :goes => :high do
+  pp "Pin is hot!"
   # speaker.play 'http://asset.uri.com/lib/fresh-pots/grohl-1.mp3'
+  Curl.post('https://hooks.slack.com/services/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', {:payload => 'payload={"text": "/giphy FRESH POTS"}'})
 end
 
 PiPiper.wait
