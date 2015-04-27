8f6653c3755c8103c2d3188f5457e4997fbc80a7
Ryan Hall <ryan.hall@vidyard.com>
2015-04-27 13:43:22 -0400

    load audio from public folder

--- lib/freshest-pot.rb
+++ lib/freshest-pot.rb
@@ -4,14 +4,23 @@ require 'sonos'
 require 'curb'
 include PiPiper
 
+# Initialize audio announcements
+audio = []
+audio_index = 0
+Dir.foreach('/home/pi/coffee/FreshPots/public') do |file|
+  next if file =='.' || file == '..'
+  audio.push(file)
+end
+audio.sort!
+
 # Speaker setup
-# system = Sonos::System.new
-# speaker = system.speakers.first
+system = Sonos::System.new
+speaker = system.speakers.first
 
 after :pin => 4, :goes => :high do
-  pp "Pin is hot!"
-  # speaker.play 'http://asset.uri.com/lib/fresh-pots/grohl-1.mp3'
+  speaker.play 'http://127.0.0.1/' + audio[audio_index]
   Curl.post('https://hooks.slack.com/services/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', {:payload => 'payload={"text": "/giphy FRESH POTS"}'})
+  audio_index = (audio_index + 1) % audio.length
 end
 
 PiPiper.wait
