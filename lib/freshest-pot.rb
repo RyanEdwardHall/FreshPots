56a38d29dd66a3e760bb04b5af3d0b7f3fb4abb1
Ryan Hall <ryan.hall@vidyard.com>
2015-04-29 14:19:04 -0400

    fix curl

--- lib/freshest-pot.rb
+++ lib/freshest-pot.rb
@@ -19,7 +19,7 @@ speaker = system.speakers.first
 
 after :pin => 4, :goes => :high do
   speaker.play 'http://127.0.0.1/' + audio[audio_index]
-  Curl.post('https://hooks.slack.com/services/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', {:payload => 'payload={"text": "/giphy FRESH POTS"}'})
+  Curl.post('https://hooks.slack.com/services/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', {:payload => '{"text": "Something\'s brewing ..."}'})
   audio_index = (audio_index + 1) % audio.length
 end
 
