8584ac1fa85eb10cd117fb8429fcd12deea8663b
Ryan Hall <ryan.hall@vidyard.com>
2015-05-01 12:37:23 -0400

    sonos now plays the file, rather than just adding to the queue. still needs a tweak

--- lib/freshest-pot.rb
+++ lib/freshest-pot.rb
@@ -1,13 +1,15 @@
+# sudo python -m SimpleHTTPServer 80
 # require 'eventmachine'
 require 'pi_piper'
 require 'sonos'
 require 'curb'
 include PiPiper
 
-brewing = false
+brewing = true
 # Initialize audio announcements
 audio = []
 audio_index = 0
+
 Dir.foreach('/home/pi/coffee/FreshPots/public') do |file|
   next if file =='.' || file == '..'
   audio.push(file)
@@ -28,8 +30,9 @@ speaker = system.speakers.first
 after :pin => 4, :goes => :low do
   if brewing
     brewing = false
-    speaker.play 'http://127.0.0.1/' + audio[audio_index]
-    Curl.post('https://hooks.slack.com/services/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', {:payload => '{"text": "FRESH POTS!"}'})
+    Curl.post('https://hooks.slack.com/services/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', {:payload => '{"text": "/giphy fresh pots"}'})
+    speaker.play 'http://raspberrypi/' + audio[audio_index]
+    speaker.play
     audio_index = (audio_index + 1) % audio.length
   end
 end
